fd0d9dee18a88727effd51fd84a82c472f8fa5a4
nafees87n <nafees87n@gmail.com>
2025-04-23 12:06:07 +0530

    fix: listener only updates blockConfig

--- app/src/hooks/DbListenerInit/userNodeListener.js
+++ app/src/hooks/DbListenerInit/userNodeListener.js
@@ -2,48 +2,43 @@ import { onValue } from "firebase/database";
 import Logger from "lib/logger";
 import { getNodeRef } from "../../actions/FirebaseActions";
 import { globalActions } from "store/slices/global/slice";
+import { getUser } from "backend/user/getUser";
 import { doc, getFirestore, onSnapshot } from "firebase/firestore";
 import firebaseApp from "firebase";
 
-const updateUserProfileInRedux = (dispatch) => {
-  if (userDetails) {
-    dispatch(
-      globalActions.updateUserProfile({
-        userProfile: profile
-          ? {
-              ...(userDetails ?? {}),
-              displayName: profile.displayName,
-              isEmailVerified: profile.isVerified || userDetails.isEmailVerified,
-              blockConfig: profile["block-config"],
-            }
-          : userDetails,
-      })
-    );
-
-    // set isSyncEnabled in window so that it can be used in storageService
-    window.isSyncEnabled = userDetails.isSyncEnabled || null;
-  }
-};
-
-// TODO: FIx the listeners here
-let profile = null;
-let userDetails = null;
 const userNodeListener = (dispatch, uid) => {
   if (uid) {
     try {
+      console.log("!!!debug", "uid", uid);
       const db = getFirestore(firebaseApp);
-
       onSnapshot(doc(db, "users", uid), (doc) => {
         if (doc.exists()) {
-          profile = doc.data();
-          updateUserProfileInRedux(dispatch);
+          const userDetails = doc.data();
+          dispatch(globalActions.updateUserBlockConfig(userDetails?.["block-config"]));
         }
       });
 
       const userNodeRef = getNodeRef(["users", uid, "profile"]);
       onValue(userNodeRef, async (snapshot) => {
-        userDetails = snapshot.val();
-        updateUserProfileInRedux(dispatch);
+        const userDetails = snapshot.val();
+        if (userDetails) {
+          getUser(uid).then((profile) => {
+            dispatch(
+              globalActions.updateUserProfile({
+                userProfile: profile
+                  ? {
+                      ...(userDetails ?? {}),
+                      displayName: profile.displayName,
+                      isEmailVerified: profile.isVerified || userDetails.isEmailVerified,
+                    }
+                  : userDetails,
+              })
+            );
+          });
+
+          // set isSyncEnabled in window so that it can be used in storageService
+          window.isSyncEnabled = userDetails.isSyncEnabled || null;
+        }
       });
     } catch (e) {
       Logger.log(e);
