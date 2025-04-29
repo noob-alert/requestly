c61f97d9d86d9cc30d848f7bb02e724c23a54629
nafees87n <nafees87n@gmail.com>
2025-04-29 20:08:03 +0530

    added userAttributes

--- app/src/hooks/ThirdPartyIntegrationsHandler.js
+++ app/src/hooks/ThirdPartyIntegrationsHandler.js
@@ -2,13 +2,21 @@ import { useCallback, useEffect } from "react";
 import firebaseApp from "firebase.js";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { initIntegrations } from "modules/analytics";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getEmailType } from "utils/mailCheckerUtils";
 import { getUser } from "backend/user/getUser";
+import { getUserAttributes } from "store/selectors";
+import { getAvailableBillingTeams } from "store/features/billing/selectors";
+import { getUserAuthDetails } from "store/slices/global/user/selectors";
+import edsIntegration from "modules/analytics/integrations/eds";
 
 const ThirdPartyIntegrationsHandler = () => {
   const dispatch = useDispatch();
   const stableDispatch = useCallback(dispatch, [dispatch]);
+
+  const userAttributes = useSelector(getUserAttributes);
+  const billingTeams = useSelector(getAvailableBillingTeams);
+  const user = useSelector(getUserAuthDetails);
 
   useEffect(() => {
     const auth = getAuth(firebaseApp);
@@ -17,12 +25,23 @@ const ThirdPartyIntegrationsHandler = () => {
         const emailType = await getEmailType(user.email);
         const userDetails = await getUser(user.uid);
         const browserstackId = userDetails?.browserstackId ?? "";
-        initIntegrations({ ...user, emailType, browserstackId }, stableDispatch);
+        initIntegrations({ ...user, emailType, browserstackId }, userAttributes, stableDispatch);
       } else {
         initIntegrations(user, stableDispatch);
       }
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [stableDispatch]);
+
+  useEffect(() => {
+    const billingDetails = {
+      billing_id: billingTeams.map((team) => team.id),
+      plan_name: user?.details?.planDetails?.planName,
+      status: user?.details?.planDetails?.status,
+    };
+
+    edsIntegration.setData(userAttributes, billingDetails);
+  }, [billingTeams, user?.details?.planDetails?.planName, user?.details?.planDetails?.status, userAttributes]);
 };
 
 export default ThirdPartyIntegrationsHandler;
