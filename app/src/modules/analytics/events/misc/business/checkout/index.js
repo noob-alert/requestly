b061711950af72c1301970d6a2d627d9e5bd667c
Parth Bhardwaj <parthb.git@gmail.com>
2025-04-30 11:32:49 +0530

    remove event

--- app/src/modules/analytics/events/misc/business/checkout/index.js
+++ app/src/modules/analytics/events/misc/business/checkout/index.js
@@ -1,11 +1,6 @@
 import { trackEvent } from "modules/analytics";
 import { BUSINESS } from "../../constants";
 
-export const trackCheckoutButtonClicked = (duration, plan, quantity, isTrialUser, source) => {
-  const params = { duration, plan, quantity, source, is_user_on_trial: isTrialUser };
-  trackEvent(BUSINESS.CHECKOUT.BUTTON_CLICKED, params);
-};
-
 export const trackCheckoutFailedEvent = (quantity, source, transaction_source) => {
   trackEvent(BUSINESS.CHECKOUT.FAILED, { quantity, source, transaction_source });
 };
